ebefb1b6dbed0c06325ff6319e415d3fb8611b07
Alexander3006 <ukrpresident2036@gmail.com>
2021-03-05 13:16:19 +0200

    add: compare speed fft vs dft

--- lab-2/index.js
+++ lab-2/index.js
@@ -12,10 +12,57 @@ const signal = generateSignal(signalHarmonics, frequency, disRepetitions);
 const dftSpectrum = complexToReal(dft(signal.y));
 const fftSpectrum = complexToReal(fft(signal.y));
 
+const measure = (testedFunc, testedParams = [], iterations = 1) => {
+  const report = {
+    x: [],
+    y: [],
+    setPoint(x, y) {
+      this.x.push(x);
+      this.y.push(y);
+      return this;
+    },
+  };
+  testedParams.map((param) => {
+    const startTime = new Date().getTime();
+    for (let i = 0; i < iterations; i++) {
+      testedFunc(param);
+    }
+    const endTime = new Date().getTime();
+    const time = endTime - startTime;
+    report.setPoint(param, time);
+  });
+  return report;
+};
+
+//Генерація сигналів довжиною 2^1 .. 2^11(FFT підтримує обрахунки сишналів, довжина, яких становить 2^n)
+const testedParams = new Array(12)
+  .fill(0)
+  .map((_, i) => generateSignal(signalHarmonics, frequency, Math.pow(2, i)).y);
+const dftTime = measure(
+  (signal) => complexToReal(dft(signal)),
+  testedParams,
+  5,
+);
+const fftTime = measure(
+  (signal) => complexToReal(fft(signal)),
+  testedParams,
+  5,
+);
+
 const chartStyle = {line: 'red', text: 'blue', baseline: 'black'};
 
-new Screen(3)
+new Screen(2)
   .addChart(chartStyle, signal, 'Random Signal', {minY: -4, maxY: 4})
   .addChart(chartStyle, {x: signal.x, y: dftSpectrum}, 'DFT')
   .addChart(chartStyle, {x: signal.x, y: fftSpectrum}, 'FFT')
+  .addChart(
+    chartStyle,
+    {x: dftTime.x.map((_) => _.length + ''), y: dftTime.y},
+    'DFT',
+  )
+  .addChart(
+    chartStyle,
+    {x: fftTime.x.map((_) => _.length + ''), y: fftTime.y},
+    'FFT',
+  )
   .render();
